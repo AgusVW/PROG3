4cfa7f4b482034b3684c7d5277e4a39439f591cf
Agustin Van Waarde <agusvanwaarde2006@gmail.com>
2025-05-10 00:25:00 -0300

    avances en recorrido BFS

--- src/tp4_grafos/Recorrido.java
+++ src/tp4_grafos/Recorrido.java
@@ -1,24 +1,28 @@
 package tp4_grafos;
+import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Iterator;
 
 
 public class Recorrido {
-    private HashMap<Integer,String> verticesColores;
+    private HashMap<Integer,String> verticesRecorridos;
+    private LinkedList<Integer> fila;
 
     public Recorrido(){
-        this.verticesColores=new HashMap<>();
+        this.verticesRecorridos=new HashMap<>();
+        this.fila=new LinkedList<>();
     }
 
-    public void recorridoDepthFirstSearch(GrafoDirigido<Integer> grafo){//recorrido en amplitud
+    public void recorridoDepthFirstSearch(GrafoDirigido<Integer> grafo){//recorrido en profundidad
+        verticesRecorridos.clear();
         Iterator<Integer> iterator=grafo.obtenerVertices();
         while(iterator.hasNext()){//copio todo mi grafo en el hash con clave el vertice copiado y value el color
-            verticesColores.put(iterator.next(),"BLANCO");
+            verticesRecorridos.put(iterator.next(),"BLANCO");
         }
-        Iterator<Integer> iteratorColores=verticesColores.keySet().iterator();//pido el iterator de mi nuevo hash de vertices,colores
+        Iterator<Integer> iteratorColores=verticesRecorridos.keySet().iterator();//pido el iterator de mi nuevo hash de vertices,colores
         while(iteratorColores.hasNext()){
             Integer actual=iteratorColores.next();
-            if (verticesColores.get(actual).equals("BLANCO")) {
+            if (verticesRecorridos.get(actual).equals("BLANCO")) {
                 DFS(grafo,actual);
             }
         }
@@ -27,15 +31,36 @@ public class Recorrido {
 
     private void DFS(GrafoDirigido<Integer> grafo,Integer vertice){
         System.out.println(vertice);//imprimo cada vertices en orden que se visitan
-        verticesColores.put(vertice,"AMARILLO");//piso mi vertice poniendole el color amarillo
+        verticesRecorridos.put(vertice,"AMARILLO");//piso mi vertice poniendole el color amarillo
         Iterator<Integer> adyacentes=grafo.obtenerAdyacentes(vertice);
         while(adyacentes.hasNext()){//recorro los adyacentes del vertice donde estoy
             int actual=adyacentes.next();
-            if(verticesColores.get(actual).equals("BLANCO")){//si mi adyacente actual que es un vertice es blanco lo visito y hago todo sucesivamente
+            if(verticesRecorridos.get(actual).equals("BLANCO")){//si mi adyacente actual que es un vertice es blanco lo visito y hago todo sucesivamente
                 DFS(grafo,actual);
             }
         }
-        verticesColores.put(vertice,"NEGRO");//una vez que recorri todos mis adyacentes me pongo en negro  pisando nuevamente la clave
+        verticesRecorridos.put(vertice,"NEGRO");//una vez que recorri todos mis adyacentes me pongo en negro  pisando nuevamente la clave
+    }
+
+
+    public void recorridoBreadthFirstSearch(GrafoDirigido<Integer> grafo){//recorrido en amplitud
+        this.fila.clear();//siempre vacio la fila
+        this.verticesRecorridos.clear();//vacio mi hash
+        Iterator<Integer> verticesGrafo=grafo.obtenerVertices();
+        while(verticesGrafo.hasNext()){//completo mi hash de verticeRecorridos como DFS
+            verticesRecorridos.put(verticesGrafo.next(),"NO VISITADO");
+        }
+        Iterator<Integer> verticesArecorrer=verticesRecorridos.keySet().iterator();
+        while (verticesArecorrer.hasNext()) {//recorro mis verticesRecorridos
+            int actual=verticesArecorrer.next();
+            if(verticesRecorridos.get(actual).equals("NO VISITADO")){//si el vertice actual es no visitado lo visito(BFS)
+                BFS(grafo,actual);
+            }
+        }
+    }
+
+    private void BFS(GrafoDirigido<Integer> g,int vertice){
+        
     }
 
 
