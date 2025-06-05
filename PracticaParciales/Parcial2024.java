1934b7fcfc466786de6a0a12928adcfeff11223b
Agustin Van Waarde <agusvanwaarde2006@gmail.com>
2025-06-05 18:59:05 -0300

    comentarios sobre hastable y ejerciciLinkedList parcial 2024

--- PracticaParciales/Parcial2024.java
+++ PracticaParciales/Parcial2024.java
@@ -1,5 +1,12 @@
 import tp1.Node;
+//HashTable
+//hastable calcula el tamaño * 2 + 1 ----> el mas uno es parq que no sea potencia de 2 y no mande mayores elementos a la
+//posicion 0 y se produzcan muchas colisiones
 
+//el calculo para ver si se agranda es el tamaño del arreglo(default 11)*0,75(roh diseño)==8 elemento por default
+
+//en el caso de hastable que el tamaño default es 11,el factor de carga es 8,
+//pero sin embargo el arreglo se agranda size>cuando mi factor de carga por lo tanto cuando inserte el 9 elemento
 public class Parcial2024 {
     private Node<String> root;
     private Node<String> last;
@@ -10,7 +17,20 @@ public class Parcial2024 {
     }
 
     public void agregarAlPrincipio(String info){
-        Node<?> node=new Node(info,null);
+        Node<String> node=new Node<String>(info,null);
         node.setNext(root);
+        this.root=node;
+        if(last==null)
+            this.last=node;
+    }
+
+    public void agregarAlFinal(String info){
+        if(root!=null){
+            Node<String> nodo=new Node<String>(info,null);
+            this.last.setNext(nodo);
+            this.last=nodo;
+        }else{
+            this.agregarAlPrincipio(info);
+        }
     }
 }
