fa4df569ab59018945a392c68119b09956bf8692
Agustin Van Waarde <agusvanwaarde2006@gmail.com>
2025-06-27 18:33:20 -0300

    agregado codigo de threads catedra

---
+++ src/ThreadsTest/src/ThreadEjemplo2.java
@@ -0,0 +1,11 @@
+package ThreadsTest.src;
+
+class ThreadEjemplo2 implements Runnable {
+
+    public void run() {
+        for (int i = 0; i < 20 ; i++) 
+            System.out.println(i + " soy " + Thread.currentThread().getName());
+        System.out.println("Termina thread " + Thread.currentThread().getName());
+    }
+    
+}
